04a4eea9d491e4f2d29590a7d952ea65e1c86696
MBOUP Mouhamadou Lamine <laphisco@yahoo.fr>
2025-06-18 22:02:09 +0200

    Navbar finished as yet

--- SpotImmo/src/app/shared/font-awesome-icons.ts
+++ SpotImmo/src/app/shared/font-awesome-icons.ts
@@ -1,8 +1,9 @@
-import {faUser, IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {faCartShopping, faUser, IconDefinition} from "@fortawesome/free-solid-svg-icons";
 
 // All icons that we want to use in the App
 // Ca nous permet de ne pas charger tous les icons afin réduire l'espace
 export const fontAwesomeIcons: IconDefinition[] = [
-  faUser
+  faUser,
+  faCartShopping
 ]
 
