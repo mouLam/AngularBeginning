b90b508a88b693a765866e1b9555a0c4eec54756
MBOUP Mouhamadou Lamine <laphisco@yahoo.fr>
2025-06-18 21:23:30 +0200

    Add first Layout Navbar

--- SpotImmo/src/app/app.component.ts
+++ SpotImmo/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component, inject} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import {FaConfig, FaIconComponent, FaIconLibrary} from "@fortawesome/angular-fontawesome";
 import {fontAwesomeIcons} from "./shared/font-awesome-icons";
+import {NavbarComponent} from "./features/layout/navbar/navbar.component";
+import {FooterComponent} from "./features/layout/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FaIconComponent],
+  imports: [RouterOutlet, RouterLink, FaIconComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
