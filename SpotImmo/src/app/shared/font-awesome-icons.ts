6a60acaf943d36579d3537e3bdb63d854beb5721
MBOUP Mouhamadou Lamine <laphisco@yahoo.fr>
2025-06-19 11:31:40 +0200

    Footer finished as yet

--- SpotImmo/src/app/shared/font-awesome-icons.ts
+++ SpotImmo/src/app/shared/font-awesome-icons.ts
@@ -1,9 +1,14 @@
 import {faCartShopping, faUser, IconDefinition} from "@fortawesome/free-solid-svg-icons";
+import {faFacebook, faLinkedinIn, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons";
 
 // All icons that we want to use in the App
 // Ca nous permet de ne pas charger tous les icons afin réduire l'espace
 export const fontAwesomeIcons: IconDefinition[] = [
   faUser,
-  faCartShopping
+  faCartShopping,
+  faFacebook,
+  faTwitter,
+  faYoutube,
+  faLinkedinIn,
 ]
 
